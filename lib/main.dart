b51b09071ff52fc7f08d8dbf280281c36f2f61ad
abdulrhmanhassan <ahai.2025@gmail.com>
2024-11-08 02:32:05 +0200

    project name & main file

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Consult());
+}
+
+class Consult extends StatelessWidget {
+  const Consult({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
